2896b951e62af5e245f8940ddebd92dd0fc4c4eb
Alexander Lebedev <alexander.lebedev@gmail.com>
2022-12-10 10:20:57 +0100

    Day 10, problem 2

--- src/main/kotlin/Monitor.kt
+++ src/main/kotlin/Monitor.kt
@@ -5,18 +5,37 @@ fun main() = Clock.solve()
 private object Clock {
     fun solve() {
         val input = readInput()
+        draw(input)
+//        fun signalStength(i: Int) = i * cpuStates[i - 1].x
+//        var total = 0
+//        for (i in listOf(20, 60, 100, 140, 180, 220)) {
+//            println("$i x=${cpuStates[i].x}")
+//            println("$i: ${signalStength(i)}")
+//            total += signalStength(i)
+//        }
+//        println("Total $total")
+    }
+
+    fun draw(input: List<Instruction>) {
+        val states = getStates(input)
+        for (i in 0..239) {
+            val sprite = i % 40
+            if (kotlin.math.abs(sprite - states[i].x) <= 1) {
+                print('#')
+            } else {
+                print('.')
+            }
+            if ((i+1) % 40 == 0) {
+                print('\n')
+            }
+        }
+    }
+    fun getStates(input: List<Instruction>): List<Cpu> {
         val cpuStates = mutableListOf(Cpu(1))
         for (instruction in input) {
             cpuStates.addAll(instruction.exec(cpuStates.last()))
         }
-        fun signalStength(i: Int) = i * cpuStates[i - 1].x
-        var total = 0
-        for (i in listOf(20, 60, 100, 140, 180, 220)) {
-            println("$i x=${cpuStates[i].x}")
-            println("$i: ${signalStength(i)}")
-            total += signalStength(i)
-        }
-        println("Total $total")
+        return cpuStates
     }
 
     private fun readInput(): List<Instruction> {
