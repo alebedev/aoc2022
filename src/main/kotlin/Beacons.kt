06c7ac1c7465b098a2a75ba0e6b296916b2a390b
Alexander Lebedev <alexander.lebedev@gmail.com>
2022-12-15 09:40:14 +0100

    Day 15, problem 2

--- src/main/kotlin/Beacons.kt
+++ src/main/kotlin/Beacons.kt
@@ -1,15 +1,44 @@
 import kotlin.math.abs
 
-fun main() = Beacons.solve(2000000)
+fun main() = Beacons.solve(0..4_000_000)
 
 private object Beacons {
-    fun solve(targetRow: Int) {
-        val beacons = readInput()
-        val ranges: List<IntRange> = rangesAtRow(beacons, targetRow)
+    fun solve(possibleCoordinates: IntRange) {
+        val sensors = readInput()
+        val distress = findDistressBeacon(sensors, possibleCoordinates)
+        println("Distress breacon at $distress, freq: ${distress.x.toBigInteger() * 4_000_000.toBigInteger() + distress.y.toBigInteger()}")
+    }
+
+    private fun findDistressBeacon(sensors: List<Sensor>, possibleCoordinates: IntRange): Pos {
+        for (y in possibleCoordinates) {
+            val ranges: List<IntRange> = rangesAtRow(sensors, y)
+            var prev: IntRange? = null
+            for (range in ranges) {
+                if (prev == null) {
+                    prev = range
+                    continue
+                }
+                if (range.last <= prev.last) {
+                    continue
+                } else if (range.first <= prev.last) {
+                    prev = prev.first..range.last
+                } else {
+                    if (prev.last in possibleCoordinates) {
+                        return Pos(prev.last + 1, y)
+                    }
+                }
+            }
+        }
+        throw Error("Distress beacon not found")
+    }
+
+    // Problem 1
+    private fun solveIntersections(sensors: List<Sensor>, targetRow: Int) {
+        val ranges: List<IntRange> = rangesAtRow(sensors, targetRow)
         // Calculate intersections
         var result = 0
         var currentRange: IntRange? = null
-        for (range in ranges.sortedBy { it.start }) {
+        for (range in ranges) {
             if (currentRange == null) {
                 currentRange = range
             } else {
@@ -41,8 +70,8 @@ private object Beacons {
     private fun rangesAtRow(sensors: List<Sensor>, targetRow: Int): List<IntRange> =
         sensors.filter { abs(targetRow - it.pos.y) <= it.distanceToBeacon() }.map {
             val dx = it.distanceToBeacon() - abs(targetRow - it.pos.y)
-            (it.pos.x - dx) until (it.pos.x + dx)
-        }
+            (it.pos.x - dx)..(it.pos.x + dx)
+        }.sortedBy { it.start }
 
     data class Pos(val x: Int, val y: Int) {
         fun distanceTo(other: Pos): Int = abs(x - other.x) + abs(y - other.y)
