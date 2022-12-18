bb9b80ab99bf1c8afe7836af4c6a2cda14cc3b53
Alexander Lebedev <alexander.lebedev@gmail.com>
2022-12-18 12:23:04 +0100

    Day 18, problem 2

--- src/main/kotlin/Droplets.kt
+++ src/main/kotlin/Droplets.kt
@@ -4,7 +4,8 @@ private object Droplets {
     fun solve() {
         val input = readInput()
         val droplet = Droplet(input)
-        println("total surface area ${droplet.surfaceArea()}")
+        println("total surface area ${droplet.surfaceArea1()}")
+        println("outside surface area ${droplet.surfaceArea2()}")
     }
 
     private fun readInput(): List<Pos> = generateSequence(::readLine).map {
@@ -22,7 +23,7 @@ private object Droplets {
     }
 
     class Droplet(val points: List<Pos>) {
-        fun surfaceArea(): Int {
+        fun surfaceArea1(): Int {
             var area = 0
 
             val pointSet = points.toSet()
@@ -31,6 +32,35 @@ private object Droplets {
             }
 //            println("${joined.size} $area")
             return area
+        }
+
+        fun surfaceArea2(): Int {
+            val minX = points.minOf { it.x }
+            val maxX = points.maxOf { it.x }
+            val minY = points.minOf { it.y }
+            val maxY = points.maxOf { it.y }
+            val minZ = points.minOf { it.z }
+            val maxZ = points.maxOf { it.z }
+            val xRange = (minX - 1)..(maxX + 1)
+            val yRange = (minY - 1)..(maxY + 1)
+            val zRange = (minZ - 1)..(maxZ + 1)
+            println("$xRange $yRange $zRange")
+            val outsideCells = mutableSetOf<Pos>()
+            val pointsSet = points.toSet()
+            val q = mutableListOf(Pos(xRange.start, yRange.start, zRange.start))
+            while (q.isNotEmpty()) {
+                val cell = q.removeFirst()
+                if (cell in pointsSet || cell in outsideCells) {
+                    continue
+                }
+                outsideCells.add(cell)
+                q.addAll(cell.sides().filter { it.x in xRange && it.y in yRange && it.z in zRange })
+            }
+            var result = 0
+            for (point in pointsSet) {
+                result += point.sides().count { it in outsideCells }
+            }
+            return result
         }
     }
 }
